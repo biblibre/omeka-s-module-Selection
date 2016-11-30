b95345216ee3e0920a7582d45d43e314dae5b19c
Julian Maurice <julian.maurice@biblibre.com>
2016-11-30 10:25:43 +0100

    Add french translation

--- config/module.config.php
+++ config/module.config.php
@@ -61,4 +61,14 @@ return  [
             'ViewJsonStrategy',
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type'        => 'gettext',
+                'base_dir'    => __DIR__ . '/../language',
+                'pattern'     => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
 ];
