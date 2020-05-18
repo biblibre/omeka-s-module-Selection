c5bb02a7f0c6c7a6576f3fbc660a254a5d2b67e9
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-05-18 00:00:00 +0000

    Added a way to pass options for the helper showSelectionLink.

--- src/View/Helper/ShowSelectionLink.php
+++ src/View/Helper/ShowSelectionLink.php
@@ -7,13 +7,21 @@ use Zend\View\Helper\AbstractHelper;
 class ShowSelectionLink extends AbstractHelper
 {
     /**
+     * The default partial view script.
+     */
+    const PARTIAL_NAME = 'common/selection-link';
+
+    /**
      * Get the link to the user selection.
      *
+     * @param array $options Options for the partial.
      * @return string
      */
-    public function __invoke()
+    public function __invoke(array $options = [])
     {
         $view = $this->getView();
-        return $view->partial('common/selection-link');
+        $template = isset($options['template']) ? $options['template'] : self::PARTIAL_NAME;
+        unset($options['template']);
+        return $view->partial($template, $options);
     }
 }
