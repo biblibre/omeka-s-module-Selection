847dcd1a283905c563ad72563ab93ed12c7ec98e
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Used Omeka S v4 feature "element group" for settings.

--- src/Form/SiteSettingsFieldset.php
+++ src/Form/SiteSettingsFieldset.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 namespace Selection\Form;
 
 use Laminas\Form\Element;
@@ -11,14 +12,20 @@ class SiteSettingsFieldset extends Fieldset
      */
     protected $label = 'Selection module'; // @translate
 
+    protected $elementGroups = [
+        'selection' => 'Selection', // @translate
+    ];
+
     public function init(): void
     {
         $this
             ->setAttribute('id', 'selection')
+            ->setOption('element_groups', $this->elementGroups)
             ->add([
                 'name' => 'selection_visitor_allow',
                 'type' => Element\Checkbox::class,
                 'options' => [
+                    'element_group' => 'selection',
                     'label' => 'Enable session selection for visitors', // @translate
                     'info' => 'The selection is automatically saved for logged users.', // @translate
                 ],
